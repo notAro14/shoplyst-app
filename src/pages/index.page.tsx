7392f77da81a9a7ee8a17ba5885d0657d601ea48
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-10-06 19:01:57 +0200

    Change icon for ready product in list

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,5 @@
 import { FC } from "react"
-import {
-  Pencil1Icon,
-  Cross1Icon,
-  DrawingPinFilledIcon,
-} from "@radix-ui/react-icons"
+import { Pencil1Icon, Cross1Icon } from "@radix-ui/react-icons"
 import toast from "react-hot-toast"
 import produce from "immer"
 
@@ -35,6 +31,26 @@ const ProductButton = styled("button", {
     outlineColor: theme.colors.solid,
   },
 })
+
+const ShoppingBagIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      width="1em"
+      height="1em"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+      />
+    </svg>
+  )
+}
 
 const IndexPage: FC = () => {
   // TODO: temporary
@@ -166,7 +182,7 @@ const ProductInsideList: FC<{
             : undefined),
         }}
       >
-        {status === "PURCHASED" && <DrawingPinFilledIcon />}
+        {status === "PURCHASED" && <ShoppingBagIcon />}
         {/*{status === "READY" && <DrawingPinIcon />}*/}
         <Text
           noLineHeight
